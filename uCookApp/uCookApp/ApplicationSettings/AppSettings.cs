97c49cde9dd988159bfed9cd7048094e27cce129
cmrnknt <cmrnknt@gmail.com>
2017-10-27 02:06:51 +0200

    First Commit
    Did some work on the Dal layer and hooked up to the controllers

---
+++ uCookApp/uCookApp/ApplicationSettings/AppSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using uCookApp.Constants;
+using uCookApp.Interfaces;
+
+namespace uCookApp.ApplicationSettings
+{
+    public static class AppSettings
+    {
+        public static string GetDBConnectionString()
+        {
+            return ReadSetting(SettingKeyNames.DBConnectionStringKey);
+        }
+
+        private static string ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "Not Found";
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                return "Error reading app settings";
+            }
+        }
+
+    }
+}
